ce185e560b3897e1750871b65e6478d09d258013
DungNguyen2402 <117297846+DungNguyen2402@users.noreply.github.com>
2023-10-27 15:36:45 +0700

    first

---
+++ origin/DTneClc4Gq/static.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<title></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+	<meta name="displayheight" content="31, 60, 90, 125, 250, 400, 600" />
+	<link rel="stylesheet" href="/css/main.css" />
+	<!--[if lt IE 9]>
+	<script src="https://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<script src="https://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+	<![endif]-->
+</head>
+<body>
+
+<a href="https://porkbun.com/link?l=f-mwp-index&utm_source=&utm_medium=mwp&utm_campaign=free&utm_content=index" target="_NEW">
+	<h1><span>Free</span> <span>Static Hosting</span></h1>
+	<h2><span>Powered by</span> <span>Porkbun.com</span></h2>
+	<img src="/images/Hero_Shared_hosting.svg"/>
+</a>
+
+</body>
+</html>
